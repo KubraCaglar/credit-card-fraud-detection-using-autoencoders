6aa8a0b21b5c9451a35ebba83491b3810afcc1d1
kubracaglar <caglarhkubra@gmail.com>
2025-05-16 13:18:28 +0300

    Add smell1 folder with updated files

---
+++ smell1/file1_obvious_vague.py
@@ -0,0 +1,12 @@
+
+# define a function
+def multiply(a, b):
+    return a * b  # returns the product of a and b
+
+# incrementing value
+x = 10
+x += 1  # add 1 to x
+
+# do something
+def process():
+    print("Processing...")  # prints a message
